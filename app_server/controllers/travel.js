4f6365283a396bd406b45c6898bc85bb10b8aa72
Jantzen <springerjantzen@yahoo.com>
2025-03-17 21:30:37 -0400

    Module 3 Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,9 @@
+var fs = require('fs')
+var trips = JSON.parse(fs.readFileSync('./data/travel.json', 'utf-8'))
 /* GET TRAVEL VIEW */
 
 const travel = (req, res) => {
     console.log('Travel route accessed')
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 module.exports = {travel};

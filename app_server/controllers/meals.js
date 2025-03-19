80a8dbd807462dc0c1b7cc3f0b3afaa0f9e73a48
Jayjai <springerjantzen@yahoo.com>
2025-03-18 20:07:23 -0400

    Dynamic rendering meals.hbs and rooms.hbs

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,8 +1,10 @@
+var fs = require('fs')
+var items = JSON.parse(fs.readFileSync('./data/meals.json', 'utf-8'))
 /* GET MEALS*/
 
 const meals = (req, res) => {
     console.log('Meals route accessed')
-    res.render('meals', {title: "Travlr Getaways"});
+    res.render('meals', {title: "Travlr Getaways", items});
 };
 
 module.exports = {meals}
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,8 +1,10 @@
+var fs = require('fs')
+var options = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf-8'))
 /* GET ROOMS*/
 
 const rooms = (req, res) => {
-    console.log('Rooms route accessed')
-    res.render('rooms', {title: "Travlr Getaways"});
+    console.log('Rooms route accessed');
+    res.render('rooms', {title: "Travlr Getaways", options});
 };
 
 module.exports = {rooms}

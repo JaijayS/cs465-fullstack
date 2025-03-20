4f4abccead529b3d434e1be767972215b421baee
Jantzen <springerjantzen@yahoo.com>
2025-03-20 09:06:52 -0400

    updates/dynamic-rendering-news.hbs

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,8 +1,11 @@
-/* GET NEWS*/
+var fs = require('fs');
+var items = JSON.parse(fs.readFileSync('./data/news.json', 'utf-8'));
 
+/* GET NEWS */
 const news = (req, res) => {
-    console.log('News route accessed')
-    res.render('news', {title: "Travlr Getaways"});
+    console.log('News route accessed');
+    res.render('news', { title: "Travlr Getaways", items });
+
 };
 
 module.exports = {news}
